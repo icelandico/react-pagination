45bff7152556697c51f558185750a635725c75b5
Michal Muszynski <m.muszy@gmail.com>
2021-04-16 19:53:02 +0200

    Setup base react project

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="main__container">
+      <h1>Pagination</h1>
+    </div>
+  );
+}
+
+export default App;
